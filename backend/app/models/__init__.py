cff52058ea945561a7032221be57b8650ce33838
LuchoLatella <lucho_latella@hotmail.com>
2026-06-03 18:58:52 -0300

    corregido el signal

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.user import User
 from app.models.asset import Asset
 from app.models.asset import Watchlist
 from app.models.asset import WatchlistItem
+from app.models.signal import AISignal
