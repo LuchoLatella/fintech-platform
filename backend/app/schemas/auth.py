6c10f047e058f1b813f2ca7fcc1e1e7f8037a29e
LuchoLatella <lucho_latella@hotmail.com>
2026-06-02 22:07:21 -0300

    corregido el schema auth

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,14 +1,17 @@
 from pydantic import BaseModel, EmailStr
 
+
 class RegisterRequest(BaseModel):
-email: EmailStr
-username: str
-password: str
+    email: EmailStr
+    username: str
+    password: str
+
 
 class LoginRequest(BaseModel):
-email: EmailStr
-password: str
+    email: EmailStr
+    password: str
+
 
 class TokenResponse(BaseModel):
-access_token: str
-token_type: str = "bearer"
+    access_token: str
+    token_type: str = "bearer"
